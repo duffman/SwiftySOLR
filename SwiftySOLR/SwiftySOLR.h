2826893879f01e6eaccd374d1b241e6361a9bceb
Patrik Forsberg <patfor@gmail.com>
2017-01-28 23:31:38 +0100

    Initial commit

---
+++ SwiftySOLR/SwiftySOLR.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySOLR.h
+//  SwiftySOLR
+//
+//  Created by Patrik Forsberg on 28/01/17.
+//  Copyright © 2017 Coldmind Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySOLR.
+FOUNDATION_EXPORT double SwiftySOLRVersionNumber;
+
+//! Project version string for SwiftySOLR.
+FOUNDATION_EXPORT const unsigned char SwiftySOLRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySOLR/PublicHeader.h>
+
+
